46523958ebdd44648b9ce9b1ce302c294cf38bed
kocery <kocery@users.noreply.github.com>
2024-10-23 05:31:55 +0000

    Deploying to gh-pages from @ kocery/OOP@f7c5a684400c81f92c08d0d4b6590254a47f5cd3 🚀

---
+++ Task_1_3_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"substringfinder","c":"SubstringFinder","l":"findSubstringOccurrences(String, String)","u":"findSubstringOccurrences(java.lang.String,java.lang.String)"},{"p":"substringfinder","c":"SubstringFinder","l":"SubstringFinder()","u":"%3Cinit%3E()"}];updateSearchResults();
